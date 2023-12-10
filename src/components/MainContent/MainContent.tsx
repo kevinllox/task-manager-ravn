17817b6bd39665e0f25072076c569c65648a8456
Kevin Grande <kevinbussines2020@gmail.com>
2023-12-10 00:21:18 -0600

    ADD: buttons container to main content

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,42 +1,18 @@
-import { Container, Flex, HStack, IconButton, Button } from '@chakra-ui/react';
-import { HiMiniSquares2X2, HiBars3, HiPlus } from 'react-icons/hi2';
+import { Container, useDisclosure } from '@chakra-ui/react';
 
 import SearchBar from '../SearchBar/SearchBar';
 import Task from '../Task/Task';
+import ButtonsContainer from '../ButtonsContainer/ButtonsContainer';
+import ModalTask from '../ModalTask/ModalTask';
 
 function MainContent() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Container maxW="100%" overflow="auto">
       <SearchBar />
-      <Flex
-        alignItems="center"
-        justifyContent="space-between"
-        paddingTop="20px"
-      >
-        <HStack>
-          <IconButton
-            aria-label="Search"
-            icon={<HiBars3 size="25px" />}
-            colorScheme="neutral"
-          />
-          <IconButton
-            aria-label="Search"
-            icon={<HiMiniSquares2X2 size="25px" />}
-            colorScheme="neutral"
-          />
-        </HStack>
-        <Button
-          bg="primary.4"
-          color="neutral.1"
-          padding="0px"
-          size="sm"
-          _hover={{ backgroundColor: 'primary.4' }}
-        >
-          <HiPlus size="20px" />
-        </Button>
-      </Flex>
-
+      <ButtonsContainer onOpen={onOpen} />
       <Task />
+      <ModalTask isOpen={isOpen} onClose={onClose} />
     </Container>
   );
 }
