69344cacb9748c80e142dd40ede230ada466be0c
Kevin Grande <kevinbussines2020@gmail.com>
2023-12-11 12:27:09 -0600

    ADD: routing in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import MainContent from './pages/MainContent/MainContent';
+import Profile from './pages/Profile/Profile';
+import { ROUTES } from './const/consts';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route element={<Dashboard />}>
+        <Route path={ROUTES.home} element={<MainContent />} />
+        <Route path={ROUTES.settings} element={<Profile />} />
+      </Route>
+      <Route path={ROUTES.notFound} element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
